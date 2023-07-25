5783b5b51085e4c06f578f13cc39f1dde2613a83
mbrignall <martin.brignall@ovo.com>
2023-07-25 18:32:57 +0100

    approach changed, new script simpler but works

--- test/test_ovoslackrandomcoffee.py
+++ test/test_ovoslackrandomcoffee.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
-import src.pyslackrandomcoffee
+import src.ovoslackrandomcoffee
 from collections import Counter
 
 
 def test_generate_pairs():
-
     def helper(members, number_of_pairs, expected_max_occurrences, previous_pairs):
-
-        pairs = src.pyslackrandomcoffee.generate_pairs(members.copy(), previous_pairs)
+        pairs = src.ovoslackrandomcoffee.generate_pairs(members.copy(), previous_pairs)
         unpacked_members = [name for pair in pairs for name in pair]
 
         # Ensure all member names are used in the pairs
@@ -24,19 +22,29 @@ def test_generate_pairs():
             assert max_occurrences == expected_max_occurrences
 
     # Uneven number of members
-    helper(['Liam', 'Olivia', 'Noah', 'Emma', 'Ava'], 3, 2, None)
+    helper(["Liam", "Olivia", "Noah", "Emma", "Ava"], 3, 2, None)
 
     # Even number of members
-    helper(['Liam', 'Olivia', 'Noah', 'Emma', 'Ava', 'Sophia'], 3, 1, None)
+    helper(["Liam", "Olivia", "Noah", "Emma", "Ava", "Sophia"], 3, 1, None)
 
     # A single member pair with one-self
-    helper(['Liam'], 1, 2, None)
+    helper(["Liam"], 1, 2, None)
 
     # No members found
     helper([], 0, 0, None)
 
     # Even with previous matches
-    helper(['Liam', 'Olivia', 'Noah', 'Emma', 'Ava', 'Sophia'], 3, 1, [[('Olivia', 'Noah'), ('Olivia', 'Ava')]])
+    helper(
+        ["Liam", "Olivia", "Noah", "Emma", "Ava", "Sophia"],
+        3,
+        1,
+        [[("Olivia", "Noah"), ("Olivia", "Ava")]],
+    )
 
     # Uneven With previous matches
-    helper(['Liam', 'Olivia', 'Emma', 'Ava', 'Sophia'], 3, 2, [[('Olivia', 'Noah'), ('Olivia', 'Ava')]])
+    helper(
+        ["Liam", "Olivia", "Emma", "Ava", "Sophia"],
+        3,
+        2,
+        [[("Olivia", "Noah"), ("Olivia", "Ava")]],
+    )
